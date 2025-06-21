5ee009ff3e1c1e8703375c1924ccfdc90ac96ab3
Luis Rosales <conejoconsal@hotmail.com>
2025-06-20 19:34:56 -0600

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,18 @@
 
 import { StarField } from "@/particles/StarField";
 
-console.log("StarField component loaded" , StarField);
+console.log("StarField component loaded", StarField);
 
 export default function Home() {
 
-  return (
-   <div className="relative h-screen overflow-hidden">
-    <StarField className="absolute inset-0 z-0"  />
-    <div className="relative z-10 flex items-center justify-center h-full">
-    <h1 className="text-white text-8xl font-bold">TRLVDSGN</h1>
-  </div>
-    </div>
-
-
-  );
+	return (
+		<div className="relative h-screen overflow-hidden">
+			<StarField className="absolute inset-0 z-0" />
+			<div className="relative z-10 flex items-center justify-center h-full">
+				<h1 className="text-white text-4xl sm:text-6xl md:text-8xl font-bold">
+					TRLVDSGN
+				</h1>
+			</div>
+		</div>
+	);
 }
